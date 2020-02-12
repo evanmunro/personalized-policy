fc01777e0172721b45e1fe265df4e924d11e983b
Evan Munro <evan.munro@gmail.com>
2020-02-12 13:52:28 -0800

    add code for manipulation case

--- model.jl
+++ model.jl
@@ -33,17 +33,34 @@ function sampleX(d1::Array{Float64})
     return x
 end
 
-function sampleX(distNull::Covariates, model)
-    dNoManip = distNull.d0
-    
-    return sampleX(distNull.d0)
+function sampleX(distNull::Covariates, model,benefit=10)
+    d1 = distNull.d0
+    manip = 0
+    # true x's
+    x = zeros(length(d1))
+    for i in 1:length(d1)
+        x[i]= sample([0,1],ProbabilityWeights([1-d1[i],d1[i]]))
+    end
+
+    # manipulated
+    for i in 1:length(d1)
+        deriv = coef(model)[i+1]
+        if x[i] == 0
+            if benefit*deriv > distNull.cost[i]
+                x[i] = 1
+                manip = 1
+            end
+        end
+    end
+    return x,manip
 end
 
-function generate_sample(N::Int64,distNull::Covariates,model=Nothing,fracH=0.3)
+function generate_sample(N::Int64,distNull::Covariates,mProb=0,model=Nothing,fracH=0.3)
     outcomeProb = ProbabilityWeights([1-fracH, fracH])
     d = distNull.d
     y = zeros(N)
     x = zeros(N,d)
+    manipulators = 0
     for i in 1:N
         y[i] = sample([0,1],outcomeProb)
         if y[i] == 1
@@ -51,12 +68,14 @@ function generate_sample(N::Int64,distNull::Covariates,model=Nothing,fracH=0.3)
             x[i,:] = sampleX(distNull.d1)
         else
             # low types might manipulate, take that into account
-            if model == Nothing
+            knows = sample([0,1],ProbabilityWeights([1-mProb,mProb]))
+            if (model == Nothing) || knows== 0
                 x[i,:] = sampleX(distNull.d0)
             else
-                x[i,:] = sampleX(distNull,model)
+                x[i,:],manip = sampleX(distNull,model)
+                manipulators += manip
             end
         end
     end
-    return x,y
+    return x,y, manipulators
 end
